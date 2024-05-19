95afe4ba64c225f30dff624cfbe7202308c02a15
wsanon19 <86744698+wsanon19@users.noreply.github.com>
2024-05-19 13:17:14 +0200

    Update workernodesetup.sh
    correction of  -y position in kubeadm install

--- Kubernetes/workernodesetup.sh
+++ Kubernetes/workernodesetup.sh
@@ -42,7 +42,7 @@ systemctl restart containerd.service
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key |gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" |tee /etc/apt/sources.list.d/kubernetes.list 
 apt update 
-sudo apt-get install –y kubelet kubeadm kubectl
+sudo apt-get install kubelet kubeadm kubectl –y
 sudo apt-mark hold kubelet kubeadm kubectl
 systemctl restart kubelet.service
 systemctl enable kubelet.service
