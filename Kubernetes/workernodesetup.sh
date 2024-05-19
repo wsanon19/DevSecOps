5e1fc4deae78fb74327165ed92fbce74d0c8cdb8
wsanon19 <86744698+wsanon19@users.noreply.github.com>
2024-05-19 19:14:03 +0200

    Update workernodesetup.sh

--- Kubernetes/workernodesetup.sh
+++ Kubernetes/workernodesetup.sh
@@ -59,13 +59,10 @@ sudo useradd \
 --no-create-home \
 --shell /bin/false node_exporter
 
-
-#Install nodes exporter packages
+#Install nodes exporter 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
 tar -xvf node_exporter-1.7.0.linux-amd64.tar.gz
 sudo mv node_exporter-1.7.0.linux-amd64/node_exporter /usr/local/bin/
-
-sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
 #Create the systemd configuration file for node exporter.
 
@@ -76,19 +73,21 @@ sudo vim /etc/systemd/system/node_exporter.service
 Description=Node Exporter
 Wants=network-online.target
 After=network-online.target
-StartLimitIntervalSec=500
-StartLimitBurst=5
+
 [Service]
 User=node_exporter
 Group=node_exporter
 Type=simple
-Restart=on-failure
-RestartSec=10s
 ExecStart=/usr/local/bin/node_exporter \
- - collector.logind
+--collector.logind
+Restart=always
+
 [Install]
 WantedBy=multi-user.target
 
-sudo systemctl restart node_exporter
+
+#Start service
 sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
 systemctl status node_exporter.service
+
